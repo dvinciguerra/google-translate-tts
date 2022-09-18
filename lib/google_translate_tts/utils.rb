2b8fe628682510b1833f4b03ec07fb5829621bf6
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2022-09-17 21:51:15 -0300

    feat: add utils module
    Creating a new utils module to keeps all common implementations

---
+++ lib/google_translate_tts/utils.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "cgi"
+require "fileutils"
+
+module GoogleTranslateTts
+  module Utils
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      protected
+
+      def copy_file(source, destination)
+        FileUtils.cp(source, destination)
+      end
+
+      def url_encode(value)
+        CGI.escape(value)
+      end
+    end
+  end
+end
